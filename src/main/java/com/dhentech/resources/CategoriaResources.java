4a8910818229af4ea453b0f0602b6f0004d154c7
Denilson Souza <denilson_contato@outlook.com>
2021-06-07 11:32:39 -0300

    chore(domain): Implementando o domain de Categoria #2

--- src/main/java/com/dhentech/resources/CategoriaResources.java
+++ src/main/java/com/dhentech/resources/CategoriaResources.java
@@ -1,5 +1,11 @@
 package com.dhentech.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
+	
 }
