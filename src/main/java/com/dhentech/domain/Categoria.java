5de32ef45826ec77a979f3d7ae74788c82f32d37
Denilson Souza <denilson_contato@outlook.com>
2021-06-07 13:15:04 -0300

    feat(domain): aplicado anotações na categoria para criar tabela no db h2 #3

--- src/main/java/com/dhentech/domain/Categoria.java
+++ src/main/java/com/dhentech/domain/Categoria.java
@@ -2,11 +2,21 @@ package com.dhentech.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@Entity(name="categoria")
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
 	private String nome;
 
 	public Categoria() {
