094ed1f5625abb267602b1dbb81ce8c395706116
Denilson Souza <denilson_contato@outlook.com>
2021-06-08 15:03:53 -0300

    feat(Resources, Service): Aplicado o Post em categoria #14

--- src/main/java/com/dhentech/resources/CategoriaResources.java
+++ src/main/java/com/dhentech/resources/CategoriaResources.java
@@ -1,12 +1,17 @@
 package com.dhentech.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dhentech.domain.Categoria;
 import com.dhentech.services.CategoriaService;
@@ -14,15 +19,27 @@ import com.dhentech.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
-	
+
 	@Autowired
-	private CategoriaService repository;
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
-		Categoria obj = repository.find(id);
-		return ResponseEntity.ok().body(obj);
+	private CategoriaService service;
+
+	// Buscar todas as categorias sem tratamento DTO por enquanto
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll() {
+		return ResponseEntity.ok(service.findAll());
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
 	
+	@PostMapping
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/dhentech/services/CategoriaService.java
+++ src/main/java/com/dhentech/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.dhentech.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,22 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 
+	// Metodo de buscar categoria com tratamento de erro
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + ", tipo: " + Categoria.class.getName()));
+	}
+
+	// Metodo com uma lista de retorno de todas as categorias
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+
+	// metodo post
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
 	}
 
 }
