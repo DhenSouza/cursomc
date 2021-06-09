e6b6a333dbbc03b1f0aa6040e73ebefdf99bf36d
Denilson Souza <denilson_contato@outlook.com>
2021-06-09 13:14:39 -0300

    feat(ClienteInsertValidator): implementação de validação de email #23

--- src/main/java/com/dhentech/services/validations/ClienteInsertValidator.java
+++ src/main/java/com/dhentech/services/validations/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dhentech.domain.Cliente;
 import com.dhentech.domain.enums.TipoCliente;
 import com.dhentech.dtos.ClienteNewDTO;
+import com.dhentech.repositories.ClienteRepository;
 import com.dhentech.resources.exceptions.FieldMessage;
 import com.dhentech.services.validations.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existe!"));
 		}
 
 		for (FieldMessage e : list) {
